a95494b4afadb2f9588f10e0f3b0d8dfe16de75a
JHULIOSSS <156727780+JHULIOSSS@users.noreply.github.com>
2025-07-23 11:04:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+// Data alvo: 10 de julho de 2025 às 00:00:00
+const countdownDate = new Date("2025-09-06T00:00:00").getTime();
+
+function updateCountdown() {
+    const now = new Date().getTime();
+    const distance = countdownDate - now;
+
+    const countdown = document.getElementById("countdown");
+    const message = document.getElementById("message");
+
+    if (distance < 0) {
+        countdown.style.display = "none";
+        message.classList.remove("hidden");
+        return;
+    }
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").textContent = String(days).padStart(2, "0");
+    document.getElementById("hours").textContent = String(hours).padStart(2, "0");
+    document.getElementById("minutes").textContent = String(minutes).padStart(2, "0");
+    document.getElementById("seconds").textContent = String(seconds).padStart(2, "0");
+}
+
+updateCountdown();
+setInterval(updateCountdown, 1000);
+
+
+
+function copiarTexto() {
+    const texto = document.getElementById("textoCopiavel").innerText;
+    navigator.clipboard.writeText(texto).then(() => {
+        document.getElementById("mensagem").innerText = "Texto copiado com sucesso!";
+    }).catch(() => {
+        document.getElementById("mensagem").innerText = "Erro ao copiar o texto.";
+    });
+}
+
+function enviarWhatsapp() {
+    const numero = "5598984251495"; // Substitua pelo seu número com DDI e DDD (ex: 55 + 99 + número)
+    const mensagem = "O mais importante para mim é ter você ao meu lado, tornando esse dia ainda mais inesquecível. Obrigada pelo carinho e por fazer parte da minha história! 💖 Com amor,";
+
+    const url = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
+    window.open(url, '_blank');
+}
